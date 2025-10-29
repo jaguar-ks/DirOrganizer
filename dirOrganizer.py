2f98594c10492ed1ff42a4594c8c31073b64c7bb
jaguar-ks <fahdamineksouss@gmail.com>
2025-10-29 13:21:34 +0100

    [PROGRESS] : targeted the downloaded file directly next step to classify it

--- dirOrganizer.py
+++ dirOrganizer.py
@@ -6,13 +6,14 @@ from watchdog.events import FileSystemEventHandler
 
 # ----------------- CONFIGURATION -----------------
 FILE_CATEGORIES = {
-    "Images": [".jpg", ".jpeg", ".png", ".gif", ".webp", ".tiff", ".svg", ".ico"],
-    "Documents": [".pdf", ".doc", ".docx", ".txt", ".xls", ".xlsx", ".ppt", ".pptx", ".odt", ".rtf"],
-    "Archives": [".zip", ".rar", ".7z", ".tar", ".gz"],
-    "Videos": [".mp4", ".mov", ".avi", ".mkv", ".webm"],
-    "Audio": [".mp3", ".wav", ".flac", ".aac"],
-    "Setup": [".exe", ".msi", ".dmg"],
-    "Code": [".py", ".html", ".css", ".js", ".php", ".json"],
+    "Images": (".jpg", ".jpeg", ".png", ".gif", ".webp", ".tiff", ".svg", ".ico"),
+    "Documents": (".pdf", ".doc", ".docx", ".txt", ".xls", ".xlsx", ".ppt", ".pptx", ".odt", ".rtf"),
+    "Archives": (".zip", ".rar", ".7z", ".tar", ".gz"),
+    "Videos": (".mp4", ".mov", ".avi", ".mkv", ".webm"),
+    "Audio": (".mp3", ".wav", ".flac", ".aac"),
+    "Setup": (".exe", ".msi", ".dmg"),
+    "Code": (".py", ".html", ".css", ".js", ".php", ".json"),
+    "Temp": ('.tmp', '.crdownload', '.part', '.download')
 }
 # -------------------------------------------------
 
@@ -42,8 +43,12 @@ class DirOrganizer:
 class DirOrganizerHandler(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
-        if not event.is_directory:
-            print(f"EVENT ACCURED: {event.event_type} [{event.src_path.split('/')[-1]}] -> [{event.dest_path.split('/')[-1]}]")
+        if not event.is_directory and event.event_type == 'moved':
+            src_path = os.path.basename(event.src_path).lower()
+            dest_path = os.path.basename(event.dest_path).lower()
+            if src_path.endswith(FILE_CATEGORIES["Temp"]):
+                print(f"File Dowloaded: [{dest_path}] is downloaded successfuly")
+            # print(f"EVENT ACCURED: {event.event_type} [{event.src_path.split('/')[-1]}] -> [{event.dest_path.split('/')[-1]}]")
 
 
 if __name__ == '__main__':
